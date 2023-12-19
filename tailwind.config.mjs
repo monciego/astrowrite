e88a013085bb56ad502aac3edd1b1c24b6941718
monciego <jerichobantiquete@gmail.com>
2023-12-20 00:52:59 +0800

    feat: show articles in homepage

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'sans-serif'],
+        sans: ["Inter", "sans-serif"],
       },
     },
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
 };
