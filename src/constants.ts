d0d89c2acd049c29d33428bd071e156fa054a2f9
monciego <jerichobantiquete@gmail.com>
2023-12-20 10:21:54 +0800

    perf: add constants for site title and description

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const SITE_TITLE = "Astrowrite";
+export const SITE_DESCRIPTION =
+  "AstroWrite is a versatile blog platform that transcends boundaries, offering an eclectic mix of engaging content spanning various subjects. From insightful commentary on current events to thought-provoking explorations of lifestyle, culture, technology, and more.";
+export const HOMEPAGE_ARTICLE_LIMIT = 14;
+export const POSTS_PER_PAGE = 6;
